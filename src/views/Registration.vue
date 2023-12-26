c59bbd80292e02cf595b1fadfbacce15cf44470c
fyk <fykkyf1223@gmail.com>
2023-12-26 14:59:16 -0500

    Register page Done

--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -4,49 +4,50 @@
       <el-form ref="patientInfoDTO" :model="patientInfoDTO" label-width="80px">
         <el-row type="flex" justify="space-around">
           <el-col :span="10">
-            <el-form-item label="患者身份证号" label-width="100px">
-              <el-input placeholder="请输入患者身份证号" v-model="patientInfoDTO.idNumber" style="width: 300px;"
+            <el-form-item label="Name" label-width="150px">
+              <el-input placeholder="Patient Name" v-model="patientInfoDTO.patientName" style="width: 300px;"></el-input>
+            </el-form-item>
+            <el-form-item label="DL" label-width="150px">
+              <el-input placeholder="Driver's License ID" v-model="patientInfoDTO.idNumber" style="width: 300px;"
                 @blur="getInfoByIdNumber()"></el-input>
             </el-form-item>
-            <el-form-item label="患者姓名" label-width="100px">
-              <el-input placeholder="请输入患者姓名" v-model="patientInfoDTO.patientName" style="width: 300px;"></el-input>
+
+            <el-form-item label="Age" label-width="150px">
+              <el-input placeholder="Patient Age" v-model="patientInfoDTO.age" style="width: 300px;"></el-input>
             </el-form-item>
-            <el-form-item label="患者年龄" label-width="100px">
-              <el-input placeholder="请输入患者年龄" v-model="patientInfoDTO.age" style="width: 300px;"></el-input>
-            </el-form-item>
-            <el-form-item label="患者性别" label-width="100px">
+            <el-form-item label="Gender" label-width="150px">
               <el-radio-group v-model="patientInfoDTO.gender" style="width: 300px;">
-                <el-radio :label="1">男</el-radio>
-                <el-radio :label="2">女</el-radio>
+                <el-radio :label="1">Male</el-radio>
+                <el-radio :label="2">Female</el-radio>
               </el-radio-group>
             </el-form-item>
-            <el-form-item label="门诊就诊编号" label-width="100px">
-              <el-select v-model="patientInfoDTO.visitorId" placeholder="请选择门诊就诊编号" style="width: 300px;"
+            <el-form-item label="Visitor ID" label-width="150px">
+              <el-select v-model="patientInfoDTO.visitorId" placeholder="Previous Visit Number" style="width: 300px;"
                 @change="getClinicDiagnoses()">
-                <el-option v-for="item in visitors" :key="item" :label="item + '号就诊号'" :value="item">
+                <el-option v-for="item in visitors" :key="item" :label="item + ' '" :value="item">
                 </el-option>
               </el-select>
             </el-form-item>
-            <el-form-item label="门诊诊断" label-width="100px">
-              <el-input placeholder="请选择门诊就诊编号" v-model="patientInfoDTO.clinicDiagnosisName" disabled
+            <el-form-item label="Previous Diagnosis" label-width="150px">
+              <el-input placeholder="" v-model="patientInfoDTO.clinicDiagnosisName" disabled
                 style="width: 300px;"></el-input>
             </el-form-item>
-            <el-form-item label="入院诊断" label-width="100px">
-              <el-select v-model="patientInfoDTO.admissionDiagnosisId" placeholder="请选择入院诊断" style="width: 300px;"
+            <el-form-item label="Admission Diagnosis" label-width="150px">
+              <el-select v-model="patientInfoDTO.admissionDiagnosisId" placeholder="" style="width: 300px;"
                 @change="getDoctorsByUnitId">
                 <el-option v-for="item in admissionDiagnoses" :key="item.diseaseId" :label="item.diseaseName" :value="item.diseaseId">
                 </el-option>
               </el-select>
             </el-form-item>
-            <el-form-item label="科室" label-width="100px">
-              <el-select v-model="patientInfoDTO.unitId" placeholder="请选择科室" style="width: 300px;"
+            <el-form-item label="Unit" label-width="150px">
+              <el-select v-model="patientInfoDTO.unitId" placeholder="" style="width: 300px;"
                 @change="getDoctorsByUnitId">
                 <el-option v-for="item in units" :key="item.unitId" :label="item.unitName" :value="item.unitId">
                 </el-option>
               </el-select>
             </el-form-item>
-            <el-form-item label="医生" label-width="100px">
-              <el-select v-model="patientInfoDTO.employeeId" placeholder="请选择医生" style="width: 300px;">
+            <el-form-item label="Physician" label-width="150px">
+              <el-select v-model="patientInfoDTO.employeeId" placeholder="" style="width: 300px;">
                 <el-option v-for="item in employees" :key="item.employeeId" :label="item.employeeName"
                   :value="item.employeeId">
                 </el-option>
@@ -56,8 +57,8 @@
         </el-row>
       </el-form>
       <span slot="footer" class="dialog-footer">
-        <el-button @click="this.patientInfoDTO = {}">取 消</el-button>
-        <el-button type="primary" @click="submitPatientInfo()">确 定</el-button>
+        <el-button @click="this.patientInfoDTO = {}">Cancel</el-button>
+        <el-button type="primary" @click="submitPatientInfo()">Submit</el-button>
       </span>
     </div>
   </div>
